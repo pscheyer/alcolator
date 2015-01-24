4f07ff7ecf0d4624fd3bf2c4b32db487b2ea51dc
Peter Scheyer <pscheyer@gmail.com>
2015-01-25 01:12:41 +0300

    Initial Commit

---
+++ Calcohol/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  Calcohol
+//
+//  Created by Peter Scheyer on 1/25/15.
+//  Copyright (c) 2015 Peter Scheyer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ Calcohol/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Calcohol
+//
+//  Created by Peter Scheyer on 1/25/15.
+//  Copyright (c) 2015 Peter Scheyer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
